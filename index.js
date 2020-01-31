a79912623905f0553bd2bd8a80bd208fd1eaf49e
Idongesit Essien <i.d.essien@gmail.com>
2020-01-30 21:05:53 -0500

    40 passing now !!

--- index.js
+++ index.js
@@ -168,8 +168,16 @@ class Instructor extends Lambdasian {
         + `PRAssignment` a method that receives a subject as an argument and returns `student.name has submitted a PR for {subject}`
         + `sprintChallenge` similar to PRAssignment but returns `student.name has begun sprint challenge on {subject}`
 */
-class Student {
-
+class Student extends Lambdasian {
+  constructor(studentian){
+    super(studentian)
+    this.previousBackground = studentian.previousBackground;
+    this.className = studentian.className;
+    this.favSubjects = studentian.favSubjects;
+  }
+  listSubjects(){
+    return this.favSubjects.toString(',');
+  }
 }
 
 /*
